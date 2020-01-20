18338fcdba079fefbf07761109ab17a00a3ef207
EetuPalo <eetu.palo@student.lab.fi>
2020-01-20 12:08:28 +0200

    Add project files.

---
+++ ASP.NET Training Project/Startup.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NET_Training_Project.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace ASP.NET_Training_Project
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("TestAuth")
+                .AddCookie("TestAuth", config => 
+                { 
+                
+                });
+
+            services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<IdentityDataContext>();
+
+            services.AddDbContext<IdentityDataContext>(cfg =>
+            {
+                cfg.UseSqlServer(configuration.GetConnectionString("IdentityDataContext"));
+            });
+
+            services.AddTransient<FormattingService>();
+
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles { 
+                DeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions")
+            });
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+
+            services.AddDbContext<BlogDataContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("BlogDataContext");
+                options.UseSqlServer(connectionString);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, FeatureToggles features)
+        {
+            app.UseExceptionHandler("/error.html");
+
+            if (features.DeveloperExceptions)
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();         
+            
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("invalid"))
+                    throw new Exception("ERROR!");
+
+                await next();
+            }
+            );
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes => {
+                routes.MapRoute("Default", "{controller=Home}/{action=Index}/{id:int?}");
+            });
+
+            app.UseFileServer();
+        }
+    }
+}
